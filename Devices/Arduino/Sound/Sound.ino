b76eadca78a45a408956321cc88256d6139a0b22
rgardler <ross@gardler.org>
2014-11-29 13:30:23 -0800

    fix time maths (and a type in the JSON)

--- Devices/Arduino/Sound/Sound.ino
+++ Devices/Arduino/Sound/Sound.ino
@@ -1,10 +1,10 @@
 // A sound level monitor using a Sunfounder Microsophone Sensor Module
 // See http://www.sunfounder.com/index.php?c=case_in&a=detail_&id=139&name=
 //
-const int SOUND_PIN = A0;  
+const int SOUND_PIN = A0;
 const int SAMPLE_FREQUENCY = 500;
 int high = 0;
-int hightime = millis();
+unsigned long hightime = millis();
 int sequenceNumber = 0;
 
 void setup() {
@@ -13,16 +13,16 @@ void setup() {
 
 void loop() {
   int value = analogRead(SOUND_PIN);
-  if(high < value) {
+  if (high < value) {
     high = value;
   }
-  if(millis() - hightime >= SAMPLE_FREQUENCY) {
+  if (millis() - hightime >= SAMPLE_FREQUENCY) {
     Serial.print("{");
     Serial.print("\"millis\":");
     Serial.print(millis());
     Serial.print(",\"seqno\":");
     Serial.print(sequenceNumber++);
-    Serial.print(",\"soundLvl:\"");
+    Serial.print(",\"soundLvl\":");
     Serial.print(high);
     Serial.println("}");
     
