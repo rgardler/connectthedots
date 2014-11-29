f60e759a7d23fd14b083c7b2ca8adcb905b37b02
rgardler <ross@gardler.org>
2014-11-29 12:24:42 -0800

    Serialize data in JSON format

--- Devices/Arduino/Sound/Sound.ino
+++ Devices/Arduino/Sound/Sound.ino
@@ -4,7 +4,8 @@
 const int SOUND_PIN = A0;  
 const int SAMPLE_FREQUENCY = 500;
 int high = 0;
-int hightime;
+int hightime = millis();
+int sequenceNumber = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -16,7 +17,15 @@ void loop() {
     high = value;
   }
   if(millis() - hightime >= SAMPLE_FREQUENCY) {
-    Serial.println(high);
+    Serial.print("{");
+    Serial.print('"millis":');
+    Serial.print(millis());
+    Serial.print(',"seqno":');
+    Serial.print(sequenceNumber++);
+    Serial.print(',"soundLvl:"');
+    Serial.print(high);
+    Serial.println("}");
+    
     high = 0;
     hightime = millis();
   }
